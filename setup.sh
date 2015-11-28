baa7fe1b3c344d7c510c85b4d2b155f68d87593c
Sandip Singh <k.sandip96@gmail.com>
2015-11-28 07:12:02 +0000

    Add jenkins installation function

--- setup.sh
+++ setup.sh
@@ -20,10 +20,51 @@ function listPuppetModules(){
 	puppet module list
 }
 
+function setHostName(){
+	# Check hostname entry
+	cat /etc/hostname |grep jenkins.example.com
+	
+	if [ $? == "0" ]; then
+		echo "Cool, HostName entry available ..."
+	else
+		echo "Adding HostName into hosts file ..."
+		echo "jenkins.example.com" > /etc/hostname
+	fi
+}
+
+function modifyHostsFile(){
+	# Check hosts file entry
+	cat /etc/hosts |grep jenkins.example.com
+	
+	if [ $? == "0" ]; then
+		echo "Cool, Hosts file entry available ..."
+	else
+		echo "Adding HostName into hosts file ..."
+		echo "127.0.1.1 jenkins.example.com" >> /etc/hosts
+	fi		
+}
+
+function installJenkins(){
+	currentDirectory=`pwd`
+	# Change dir to /etc/puppet/manifests
+	cd /etc/puppet/manifests
+	# Copy manifest file
+	cp $currentDirectory/files/jenkins.pp .
+	# Install jenkins
+	puppet apply jenkins.pp --debug
+}
+
+
+
 function main(){
 	installStandalonePuppet
 	installPuppetModules
 	listPuppetModules
+	setHostName
+	installJenkins
 }
 
 main
+
+
+
